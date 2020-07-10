d88476787c930d7d43be9a9e9c80094dbd049800
keskineva <developer@example.com>
2020-07-10 16:29:52 +0300

    initial commit

---
+++ src/test/java/softuni/meal_plan/MealPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package softuni.meal_plan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPlanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
