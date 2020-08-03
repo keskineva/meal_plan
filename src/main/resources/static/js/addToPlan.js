1ba836d6fcf9b7843273e3b37d7a298b77b692bb
keskineva <developer@example.com>
2020-08-03 21:15:36 +0300

    Ajax is returning a message now.

--- src/main/resources/static/js/addToPlan.js
+++ src/main/resources/static/js/addToPlan.js
@@ -6,11 +6,17 @@ $('.addToPlanForm').submit(function (e) {
         url: 'all?addToPlan',
         data: $(this).serialize(),
         success: function (data) {
-            console.log("Successfully added.")
+            console.log("Successfully added: " + data)
             //callback methods go right here
+            var successDiv = e.currentTarget.parentElement.querySelector(".addToPlanResult");
+            successDiv.innerHTML = "Meal added to plan successfully.";
+            successDiv.removeAttribute("hidden");
         },
         error: function (req, err) {
             console.error('Error: ' + err);
+            var errorDiv = e.currentTarget.parentElement.querySelector(".addToPlanResultErr");
+            errorDiv.innerHTML = "Ops, something went wrong. :(";
+            errorDiv.removeAttribute("hidden");
         }
     });
 
