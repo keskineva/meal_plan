ea335c953849bd5bfcd3c8953cd3a028d76ee5ec
keskineva <developer@example.com>
2020-08-07 23:01:53 +0300

    add specific message when bad credentials are given

--- src/main/java/softuni/meal_plan/web/UserController.java
+++ src/main/java/softuni/meal_plan/web/UserController.java
@@ -4,6 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -100,7 +101,11 @@ public class UserController extends BaseController {
         if (session != null) {
             Exception ex = (Exception) session.getAttribute("SPRING_SECURITY_LAST_EXCEPTION");
             if (ex != null) {
-                errorMessage = ex.getMessage();
+                if (ex instanceof BadCredentialsException) {
+                    errorMessage = "Username or password is incorrect";
+                } else {
+                    errorMessage = ex.getMessage();
+                }
             }
         }
         model.addAttribute("errorMessage", errorMessage);
