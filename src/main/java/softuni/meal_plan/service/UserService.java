b0c1bb931d779e8d4b15372943ed87fb798dadfc
keskineva <developer@example.com>
2020-08-08 11:44:01 +0300

    remove obsolete method

--- src/main/java/softuni/meal_plan/service/UserService.java
+++ src/main/java/softuni/meal_plan/service/UserService.java
@@ -26,7 +26,5 @@ public interface UserService extends UserDetailsService {
 
     UserServiceModel findByUsername(String username);
 
-    UserServiceModel register(UserServiceModel userServiceModel);
-
     UserServiceModel findUserByEmail(String email);
 }
