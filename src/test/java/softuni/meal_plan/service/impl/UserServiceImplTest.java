336e1d9c30696dc126c9b2177b6a4edfbdce4110
keskineva <developer@example.com>
2020-08-08 11:43:14 +0300

    add user service impl tests

--- src/test/java/softuni/meal_plan/service/impl/UserServiceImplTest.java
+++ src/test/java/softuni/meal_plan/service/impl/UserServiceImplTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import softuni.meal_plan.error.Constants;
 import softuni.meal_plan.model.entity.Role;
 import softuni.meal_plan.model.entity.User;
 import softuni.meal_plan.model.service.UserServiceModel;
@@ -26,7 +27,7 @@ class UserServiceImplTest {
     public static final String TEST_USER_EMAIL = "testuser@test";
     public static final String TEST_USER_ADDRESS = "Address";
     public static final String TEST_USER_NAME = "TestUser";
-    public static final String TEST_USER_PASS = "testpass";
+    public static final String TEST_USER_PASS = "$2a$10$V0j5waMYKYNcbW2wF9.4qOYfISK2pxXuAnmxidhhRQ4zZOg0ZVP8u";
     private UserRepository userRepositoryMock;
     private RoleRepository roleRepositoryMock;
     private User testUser;
@@ -40,6 +41,7 @@ class UserServiceImplTest {
         this.testUser.setEmail(TEST_USER_EMAIL);
         this.testUser.setUsername(TEST_USER_NAME);
         this.testUser.setPassword(TEST_USER_PASS);
+        this.testUser.setAuthorities(new HashSet<>());
 
         Role roleUser = new Role();
         roleUser.setAuthority("ROLE_USER");
@@ -140,7 +142,68 @@ class UserServiceImplTest {
     }
 
     @Test
-    void editUserProfile() {
+    void editUserProfile_Success() {
+        Mockito.when(userRepositoryMock.findByUsername(TEST_USER_NAME)).thenReturn(Optional.ofNullable(testUser));
+        Mockito.when(userRepositoryMock.saveAndFlush(Mockito.any(User.class))).thenAnswer(invocation -> invocation.getArguments()[0]);
+        RoleService roleService = new RoleServiceImpl(roleRepositoryMock, new ModelMapper());
+        UserService userService = new UserServiceImpl(userRepositoryMock, roleService, new ModelMapper(), new BCryptPasswordEncoder());
+        // act
+        UserServiceModel userServiceModel = new UserServiceModel();
+        userServiceModel.setUsername(TEST_USER_NAME);
+        userServiceModel.setEmail("NewEmail@email");
+        userServiceModel.setPassword("12345");
+        userServiceModel.setId(TEST_USER_ID);
+        userServiceModel.setAddress(TEST_USER_ADDRESS);
+        userServiceModel.setName(TEST_USER_JUST_NAME);
+
+        UserServiceModel actual = userService.editUserProfile(userServiceModel, "123");
+
+        // Assert
+        Assert.assertEquals("Mismatch", "NewEmail@email", actual.getEmail());
+        Assert.assertNotEquals("New password expected", TEST_USER_PASS, actual.getPassword());
+    }
+
+    @Test
+    void editUserProfile_IncorrectOldPass() {
+        Mockito.when(userRepositoryMock.findByUsername(TEST_USER_NAME)).thenReturn(Optional.ofNullable(testUser));
+        Mockito.when(userRepositoryMock.saveAndFlush(Mockito.any(User.class))).thenAnswer(invocation -> invocation.getArguments()[0]);
+        RoleService roleService = new RoleServiceImpl(roleRepositoryMock, new ModelMapper());
+        UserService userService = new UserServiceImpl(userRepositoryMock, roleService, new ModelMapper(), new BCryptPasswordEncoder());
+        // act
+        UserServiceModel userServiceModel = new UserServiceModel();
+        userServiceModel.setUsername(TEST_USER_NAME);
+        userServiceModel.setEmail("NewEmail@email");
+        userServiceModel.setPassword("12345");
+        userServiceModel.setId(TEST_USER_ID);
+        userServiceModel.setAddress(TEST_USER_ADDRESS);
+        userServiceModel.setName(TEST_USER_JUST_NAME);
+
+        Exception exception = Assert.assertThrows(IllegalArgumentException.class, () -> {
+            UserServiceModel actual = userService.editUserProfile(userServiceModel, "incorrectOldPass123");
+        });
+        Assert.assertEquals("Mismatch", Constants.PASSWORD_IS_INCORRECT, exception.getMessage());
+    }
+
+    @Test
+    void editUserProfile_EmptyPass() {
+        Mockito.when(userRepositoryMock.findByUsername(TEST_USER_NAME)).thenReturn(Optional.ofNullable(testUser));
+        Mockito.when(userRepositoryMock.saveAndFlush(Mockito.any(User.class))).thenAnswer(invocation -> invocation.getArguments()[0]);
+        RoleService roleService = new RoleServiceImpl(roleRepositoryMock, new ModelMapper());
+        UserService userService = new UserServiceImpl(userRepositoryMock, roleService, new ModelMapper(), new BCryptPasswordEncoder());
+        // act
+        UserServiceModel userServiceModel = new UserServiceModel();
+        userServiceModel.setUsername(TEST_USER_NAME);
+        userServiceModel.setEmail("NewEmail@email");
+        userServiceModel.setPassword("");
+        userServiceModel.setId(TEST_USER_ID);
+        userServiceModel.setAddress(TEST_USER_ADDRESS);
+        userServiceModel.setName(TEST_USER_JUST_NAME);
+
+        UserServiceModel actual = userService.editUserProfile(userServiceModel, "123");
+
+        // Assert
+        Assert.assertEquals("Mismatch", "NewEmail@email", actual.getEmail());
+        Assert.assertEquals("Password should not be changed...", TEST_USER_PASS, actual.getPassword());
     }
 
     @Test
@@ -163,14 +226,39 @@ class UserServiceImplTest {
 
     @Test
     void makeAdmin() {
+        Mockito.when(this.userRepositoryMock.findById(TEST_USER_ID))
+                .thenReturn(Optional.ofNullable(this.testUser));
+        Mockito.when(roleRepositoryMock.findByAndAuthority("ROLE_USER"))
+                .thenReturn(new Role("ROLE_USER"));
+        Mockito.when(roleRepositoryMock.findByAndAuthority("ROLE_ADMIN"))
+                .thenReturn(new Role("ROLE_ADMIN"));
+        Mockito.when(userRepositoryMock.saveAndFlush(Mockito.any(User.class))).thenAnswer(invocation -> invocation.getArguments()[0]);
+        RoleService roleService = new RoleServiceImpl(roleRepositoryMock, new ModelMapper());
+        UserService userService = new UserServiceImpl(userRepositoryMock, roleService, new ModelMapper(), new BCryptPasswordEncoder());
+
+        // Act
+        userService.makeAdmin(TEST_USER_ID);
+
+        // Assert
+        Mockito.verify(userRepositoryMock, Mockito.times(1)).saveAndFlush(Mockito.any(User.class));
     }
 
     @Test
     void makeUser() {
-    }
+        Mockito.when(this.userRepositoryMock.findById(TEST_USER_ID))
+                .thenReturn(Optional.ofNullable(this.testUser));
+        Mockito.when(roleRepositoryMock.findByAndAuthority("ROLE_USER"))
+                .thenReturn(new Role("ROLE_USER"));
 
-    @Test
-    void register() {
+        Mockito.when(userRepositoryMock.saveAndFlush(Mockito.any(User.class))).thenAnswer(invocation -> invocation.getArguments()[0]);
+        RoleService roleService = new RoleServiceImpl(roleRepositoryMock, new ModelMapper());
+        UserService userService = new UserServiceImpl(userRepositoryMock, roleService, new ModelMapper(), new BCryptPasswordEncoder());
+
+        // Act
+        userService.makeUser(TEST_USER_ID);
+
+        // Assert
+        Mockito.verify(userRepositoryMock, Mockito.times(1)).saveAndFlush(Mockito.any(User.class));
     }
 
     @Test
